48a260ab97687d74fa29b29e08f91d1e8c02e10e
Manuel Félix Parma <parmamanuelfelix@gmail.com>
2023-05-16 19:07:49 +0200

    Tools rotate, added dirt pile

--- Assets/Scripts/GrabTool.cs
+++ Assets/Scripts/GrabTool.cs
@@ -1,23 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GrabTool : MonoBehaviour
 {
-    //public Transform player;
-    private Collider currentHolding = null;
+    private Collider currentHolding = null; // to detect the current object being holded
+    public List<AudioSource> audioSource; // audio sounds when object is picked 
+
+    List<string> tools_idx = new List<string>();
+
+    public void Start()
+    {
+        tools_idx.Add("pickaxe");
+        tools_idx.Add("scroll");
+        tools_idx.Add("shovel");
+        tools_idx.Add("radar");
+    }
 
     public void OnTriggerEnter(Collider other)
     {
         //if (other.gameObject.CompareTag("CollideTool"))
         //{
-        Debug.Log("collision with tool ocurred");
+        //Debug.Log("collision with tool ocurred");
         if (currentHolding != null) //first iteration
         {
             currentHolding.transform.SetParent(null);
         }
         currentHolding = other;
         other.transform.SetParent(transform);
+
+        for (int i = 0; i < tools_idx.Count; i++)
+        {
+            if (other.gameObject.CompareTag(tools_idx[i]))
+            {
+                Debug.Log("Collision with" + tools_idx[i]);
+                Rotate rotationProp = other.gameObject.GetComponent<Rotate>();
+                rotationProp.isRotating = false;
+                //audioSource[ i ].Play();
+            }
+        }
+
+
         //}
     }
 }
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -6,11 +6,14 @@ public class Rotate : MonoBehaviour
 {
     public float angle = 0f;
     public float rotationSpeed = 20f;
+    public bool isRotating = true;
  
     // Update is called once per frame
     void Update()
     {
-        angle += rotationSpeed * Time.deltaTime;
-        transform.localRotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        if (isRotating) {
+            angle += rotationSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        }
     }
 }
